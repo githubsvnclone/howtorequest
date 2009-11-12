30a59ec6306be3f086e12de897522a1282e93547
githubsvnclone <githubsvnclone@li49-39.members.linode.com>
2009-11-12 15:43:06 +0000

    move

--- update.rb
+++ update.rb
@@ -1,20 +1,25 @@
 #!/usr/local/bin/ruby
-for file in Dir['*.desc']
- settings = eval(File.read(file)) # hash
- 
- Dir.chdir(settings[:dir]) do
-   command = "git-svn rebase #{settings[:repo]}"
-   if settings[:authors]
-     command = "#{command} --authors-file #{settings[:authors]}"
-   end
-   puts settings[:dir], command
-   system(command)
-   system("git push origin master")
+require 'fileutils'
 
-   # update tags by hand [sigh]
-   all = `git branch -a | grep tags`
-   all.each_line{|line| line =~ /\/(.....$)/; system("git push origin tags/#{$1}") }
- end
+Dir.chdir(File.dirname(__FILE__)) do
 
+  for file in Dir['*.desc']
+    settings = eval(File.read(file)) # hash
+
+    Dir.chdir(settings[:dir]) do
+      command = "git-svn rebase #{settings[:repo]}"
+      if settings[:authors]
+        command = "#{command} --authors-file #{settings[:authors]}"
+      end
+      puts settings[:dir], command
+      system(command)
+      system("git push origin master")
+
+      # update tags by hand [sigh]
+      all = `git branch -a | grep tags`
+      all.each_line{|line| line =~ /\/(.....$)/; system("git push origin tags/#{$1}") }
+    end
+
+  end
+  FileUtils.touch '/home/githubsvnclone/hereiam2'
 end
-
